47952d857ae79552fea99565e6097489c1fece5c
shamirashuchi <shamirashuchi@gmail.com>
2023-06-27 15:52:43 +0600

    all done

--- src/Component/Projects/Projects.jsx
+++ src/Component/Projects/Projects.jsx
@@ -18,7 +18,7 @@ const Projects = () => {
 
 
             {
-                projectsData.map(projects => <section className="container mx-auto p-10 md:py-20 px-0 md:p-10 md:px-0">
+                projectsData.map(projects => <section className="container mx-auto p-10 md:py-60 px-0 md:p-10 md:px-0">
                     <section className="relative px-10 md:p-0 transform duration-500 hover:shadow-2xl cursor-pointer hover:-translate-y-1 ">
                         <div data-aos="fade-right">
                             <img className="xl:max-w-8xl lg:mx-4 rounded-lg shadow-lg shadow-[#00df9a]" src={projects.img1} alt="" />
@@ -30,12 +30,26 @@ const Projects = () => {
                             </div>
                             <h2 className="text-3xl font-semibold mt-4 md:mt-10 text-[#00df9a]">{projects.title1}</h2>
                             <p className="my-3 text-justify font-medium  leading-relaxed ">{projects.description}</p>
-
+                            <div className='mt-4'>
+                                <h1 className=' text-2xl font-bold text-[#00df9a]'>Overview</h1>
+                            </div>
+                            <div className="px-4 mb-12 mt-8 mx-auto sm:max-w-xl md:max-w-full  lg:max-w-screen-xl md:px-24 lg:px-8">
+                                <div className="p-8 rounded shadow-xl bg-[#0b4231] shadow-[#00df9a] sm:p-16">
+                                    <div className="">
+                                        <li className="mb-4 text-white ">
+                                            {projects.fulldescription1}
+                                        </li>
+                                        <li className="mb-4 text-white ">
+                                            {projects.fulldescription2}
+                                        </li>
+                                    </div>
+                                    </div>
+                                    </div>
                             <div className="flex gap-4">
                                 <a href={projects.livelink}><FaLaptop className='text-2xl'></FaLaptop></a>
                                 <a href={projects.githublink}><FaGithub className='text-2xl'></FaGithub></a>
                                
-                                 <Link to={`project/${projects.id}`}><button className='btn btn-sm text-white btn-outline hover:bg-[#00df9a]'>Details</button></Link>
+                                 {/* <Link to={`project/${projects.id}`}><button className='btn btn-sm text-white btn-outline hover:bg-[#00df9a]'>Details</button></Link> */}
                                   
                             </div>
                         </div>
